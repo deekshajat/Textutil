b9163cef9ea961d53faad9872e6c936dbf3c216b
Deeksha <deekshajat01@gmail.com>
2024-06-21 16:01:29 +0530

    updated

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -84,7 +84,12 @@ export default function TextForm(props) {
       >
         <h2>Your Text Summary</h2>
         <p>
-          {text.split(" ").length} words and {text.length} characters
+          {
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").length} minutes read</p>
         <h2>Preview</h2>
